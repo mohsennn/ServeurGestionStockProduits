e96fb9cd13392dcddeacae0f66b671ea3ac3d114
mohsennn <mohsenghanem02@gmail.com>
2019-08-14 06:21:56 -1000

    first commit server

--- src/main/java/com/example/ServeurGestionStockProduits/ServeurGestionStockProduitsApplication.java
+++ src/main/java/com/example/ServeurGestionStockProduits/ServeurGestionStockProduitsApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication 
-public class ServeurGestionStockProduitsApplication {
+public class ServeurGestionStockProduitsApplication  {
 
 	public static void main(String[] args) {
+		System.out.println("hello world ");
 		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
 	}
 
